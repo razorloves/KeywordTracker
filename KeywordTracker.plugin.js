cf0bfb26fbc6bd172b699b4775066968a7fb359a
razorloves <razorloves@gmail.com>
2025-12-25 06:03:09 -0600

    Fix alignment of Keyword Matches button in the toolbar

--- KeywordTracker.plugin.js
+++ KeywordTracker.plugin.js
@@ -834,7 +834,7 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
 
 		// build the inbox panel placed directly after the pinned messages button
 		buildInboxPanel() {
-			let pinned = document.querySelector('div[class$="toolbar" i] > div:first-child');
+			let pinned = document.querySelector('div[class$="toolbar" i] > div[role="button"]');
 			if (!pinned) {
 				return;
 			}
